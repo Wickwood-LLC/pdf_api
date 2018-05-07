dd88246fa5cbc09f191f176bcfc8d38b04c52f65
Nigel Cunningham <nigel@grindstone.com.au>
2018-05-08 09:46:25 +1000

    Get mPDF applying page size and orientation correctly.
    Signed-off-by: Nigel Cunningham <nigel@grindstone.com.au>

--- src/Plugin/PdfGenerator/MpdfGenerator.php
+++ src/Plugin/PdfGenerator/MpdfGenerator.php
@@ -42,12 +42,31 @@ class MpdfGenerator extends PdfGeneratorBase implements ContainerFactoryPluginIn
   protected $generator;
 
   /**
+   * The saved header content.
+   *
+   * @var string
+   */
+  protected $headerContent;
+
+  /**
+   * The saved PDF content.
+   *
+   * @var string
+   */
+  protected $pdfContent;
+
+  /**
+   * The saved footer content.
+   *
+   * @var string
+   */
+  protected $footerContent;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, mPDF $generator) {
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->generator = $generator;
   }
 
   /**
@@ -57,8 +76,7 @@ class MpdfGenerator extends PdfGeneratorBase implements ContainerFactoryPluginIn
     return new static(
       $configuration,
       $plugin_id,
-      $plugin_definition,
-      $container->get('mpdf')
+      $plugin_definition
     );
   }
 
@@ -68,24 +86,11 @@ class MpdfGenerator extends PdfGeneratorBase implements ContainerFactoryPluginIn
   public function setter($pdf_content, $pdf_location, $save_pdf, $paper_orientation, $paper_size, $footer_content, $header_content, $path_to_binary = '') {
     $this->setPageSize($paper_size);
     $this->setPageOrientation($paper_orientation);
-    $this->setHeader($header_content);
-    $this->setFooter($footer_content);
-    $filename = $pdf_location;
-    $this->preGenerate();
-    $stylesheet = '.node_view  { display: none; }';
-    $this->generator->WriteHTML($stylesheet, 1);
-    $this->generator->WriteHTML(utf8_encode($pdf_content), 0);
-    if ($save_pdf) {
-      if (empty($filename)) {
-        $filename = str_replace("/", "_", \Drupal::service('path.current')->getPath());
-        $filename = substr($filename, 1);
-      }
-      $this->stream($filename . '.pdf');
-    }
-    else {
-      $this->send(utf8_encode($pdf_content));
-    }
-    $this->addPage($pdf_content);
+
+    // Save until the generator is constructed in the preGenerate method.
+    $this->headerContent = $header_content;
+    $this->pdfContent = $pdf_content;
+    $this->footerContent = $footer_content;
   }
 
   /**
@@ -187,7 +192,35 @@ class MpdfGenerator extends PdfGeneratorBase implements ContainerFactoryPluginIn
    * Set the global options from the plugin into the mPDF generator class.
    */
   protected function preGenerate() {
-    $this->generator->AddPageByArray($this->options);
+    /*
+     * We have to pass the initial page size and orientation that we want to
+     * the constructor, so we delay making the generator until we have those
+     * details.
+     *
+     * mPDF is also strange in its handling of parameters. We can't just set
+     * the page size and orientation separately (as you'd expect) but need to
+     * combine them in the format argument for them to be effective from the
+     * get-go.
+     */
+    $options = $this->options;
+
+    $config = [ ];
+    $orientation = '';
+
+    $orientation = $options['orientation'] ? $options['orientation'] : 'P';
+
+    $config['format'] = $this->isValidPageSize($options['sheet-size']) ? $options['sheet-size'] : 'A4';
+    if ($orientation == 'L') {
+      $config['format'] .= '-' . $orientation;
+    }
+
+    $this->generator = new mPDF($config);
+
+    $this->setHeader($this->headerContent);
+    $this->setFooter($this->footerContent);
+    $stylesheet = '.node_view  { display: none; }';
+    $this->generator->WriteHTML($stylesheet, 1);
+    $this->generator->WriteHTML(utf8_encode($this->pdfContent), 0);
   }
 
 }
